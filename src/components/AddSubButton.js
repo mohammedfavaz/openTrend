805565cfc2ab95799458e0f553a14ff9257d3bab
mohammedfavaz <61573924+mohammedfavaz@users.noreply.github.com>
2022-06-22 23:07:20 +0530

    Update

--- src/components/AddSubButton.js
+++ src/components/AddSubButton.js
@@ -3,13 +3,19 @@ import React,{useState} from 'react'
 import AntDesign from 'react-native-vector-icons/AntDesign'
 export default function AddSubButton() {
   const [count, setcount] = useState(10)
+  const incerement=()=>{
+    setcount(pre=>pre+1)
+  }
+  const decrement=()=>{
+    count>0&&setcount(pre=>pre-1)
+  }
   return (
     <View style={styles.mainView}>
-      <TouchableOpacity style={[styles.buttonView,{backgroundColor:"#E2F8E3"}]} activeOpacity={0.5} onPress={()=>setcount(count+1)} >
+      <TouchableOpacity style={[styles.buttonView,{backgroundColor:"#E2F8E3"}]} activeOpacity={0.5} onPress={incerement} >
       <AntDesign name={'plus'} size={15} color={"#155D18"} />
       </TouchableOpacity>
       <Text style={{fontSize:15,color:"#5B5B5B",fontWeight:"bold"}}>{count}</Text>
-      <TouchableOpacity style={[styles.buttonView,{backgroundColor:"#FDE7E7"}]} activeOpacity={0.5} onPress={()=>count>0&&setcount(count-1)} >
+      <TouchableOpacity style={[styles.buttonView,{backgroundColor:"#FDE7E7"}]} activeOpacity={0.5} onPress={decrement} >
       <AntDesign name={'minus'} size={15} color={"#920E0B"} />
       </TouchableOpacity>
     </View>
