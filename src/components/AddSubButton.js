0cebc67abc4d564d686ab9a769b64dac69f03dd6
mohammedfavaz <61573924+mohammedfavaz@users.noreply.github.com>
2022-06-22 23:15:50 +0530

    Update

--- src/components/AddSubButton.js
+++ src/components/AddSubButton.js
@@ -14,7 +14,7 @@ export default function AddSubButton() {
       <TouchableOpacity style={[styles.buttonView,{backgroundColor:"#E2F8E3"}]} activeOpacity={0.5} onPress={incerement} >
       <AntDesign name={'plus'} size={15} color={"#155D18"} />
       </TouchableOpacity>
-      <Text style={{fontSize:15,color:"#5B5B5B",fontWeight:"bold"}}>{count}</Text>
+      <Text style={styles.txt}>{count}</Text>
       <TouchableOpacity style={[styles.buttonView,{backgroundColor:"#FDE7E7"}]} activeOpacity={0.5} onPress={decrement} >
       <AntDesign name={'minus'} size={15} color={"#920E0B"} />
       </TouchableOpacity>
@@ -39,5 +39,10 @@ const styles=StyleSheet.create({
     borderRadius:7,
     alignItems:"center",
     justifyContent:"center"
-  }
+  },
+  txt:{
+    fontSize:15,
+    color:"#5B5B5B",
+    fontWeight:"bold"
+    }
 })
